e3e41458c2b2ba31ba892518483c4c785f110f6c
sheo96189 <sheo96189@gmail.com>
2024-05-04 02:18:52 +0530

    features

---
+++ src/main/java/com/ConnectionChecking/ConnectionChecking/ConnectionCheckingApplication.java
@@ -0,0 +1,15 @@
+package com.ConnectionChecking.ConnectionChecking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ConnectionCheckingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionCheckingApplication.class, args);
+		System.out.println("Hello shiv");
+	}
+
+}
---
+++ src/test/java/com/ConnectionChecking/ConnectionChecking/ConnectionCheckingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ConnectionChecking.ConnectionChecking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionCheckingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
